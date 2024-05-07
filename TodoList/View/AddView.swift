36616c74a1cf517e1bd9e45a410a4355f9b1b57e
Bandan Kumar Mahto <159515893+BkMahto@users.noreply.github.com>
2024-05-07 16:06:05 +0530

    View Model created

--- TodoList/View/AddView.swift
+++ TodoList/View/AddView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.dismiss) var dismiss
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -17,12 +21,10 @@ struct AddView: View {
                 TextField("Type Something here...", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height: 55)
-                .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
-                .cornerRadius(10)
+                    .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
+                    .cornerRadius(10)
                 
-                Button {
-                    
-                } label: {
+                Button(action: saveButtonPressed) {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
                         .font(.headline)
@@ -35,7 +37,30 @@ struct AddView: View {
             .padding(14)
         }
         .navigationTitle("Add an Item 🖊")
+        .alert(alertTitle, isPresented: $showAlert, actions: { })
+            
+        
     }
+    
+    
+    func saveButtonPressed() {
+        if textIsAppropiate() {
+            listViewModel.addItem(title: textFieldText)
+            dismiss.callAsFunction()
+        } else {
+            
+        }
+    }
+    
+    func textIsAppropiate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be atleast 3 characters long!!! 😱😰😨"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
 }
 
 struct AddView_Previews: PreviewProvider {
@@ -43,5 +68,6 @@ struct AddView_Previews: PreviewProvider {
         NavigationView {
             AddView()
         }
+        .environmentObject(ListViewModel())
     }
 }
